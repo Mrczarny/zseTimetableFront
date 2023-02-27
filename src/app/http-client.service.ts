98fcb3561b9501a8226ae3a23178732cf7a468c4
Jan Adamski <j.adamski@zsebydgoszcz.onmicrosoft.com>
2023-02-27 23:46:21 +0100

    httpClient first scaffolding

--- src/app/http-client.service.ts
+++ src/app/http-client.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import * as httpConfig from '../assets/httpConfig.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpClientService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    http.options(httpConfig.baseAddress, { responseType: 'json'})
+  }
+
+  getAllNames(type: ISelectable) {
+    return this.http.get<string[]>(`/api/${type.type}/allnames`)
+  }
+
+  getTimetable(type: ISelectable){
+    return this.http.get<ITimetable>(`${httpConfig.timetablesUrl}/${type.Name}`)
+  }
+
+  getAllCurrentChanges(){
+    return this.http.get<IChange[]>(`${httpConfig.changesUrl}/today`)
+  }
+  getChange(type: IChangable) {
+    throw Error;
+    return this.http.get<IChange>(`${httpConfig.changesUrl}/`)
+  }
 }
+
