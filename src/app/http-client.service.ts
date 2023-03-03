e8a7b3bc2e7645cfec53eeeddca922ecd020b138
Jan Adamski <j.adamski@zsebydgoszcz.onmicrosoft.com>
2023-03-03 16:12:37 +0100

    httpClient uses ISelectable

--- src/app/http-client.service.ts
+++ src/app/http-client.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
-import * as httpConfig from '../assets/httpConfig.json';
+import httpConfig from '../assets/httpConfig.json';
+import { ISelectable } from './Models/ISelectable';
 
 @Injectable({
   providedIn: 'root'
@@ -10,23 +11,23 @@ import * as httpConfig from '../assets/httpConfig.json';
 export class HttpClientService {
 
   constructor(private http: HttpClient) {
-    http.options(httpConfig.baseAddress, { responseType: 'json'})
+   // http.options(httpConfig.baseAddress, { responseType: 'json'})
   }
 
   getAllNames(type: ISelectable) {
-    return this.http.get<string[]>(`/api/${type.type}/allnames`)
+    return this.http.get<string[]>(`${httpConfig.baseAddress}/api/${type.TypeName}/allnames`)
   }
 
   getTimetable(type: ISelectable){
-    return this.http.get<ITimetable>(`${httpConfig.timetablesUrl}/${type.Name}`)
+    return this.http.get<ISelectable>(`${httpConfig.baseAddress}/api/${httpConfig.timetablesUrl}/${type.Name}`)
   }
 
-  getAllCurrentChanges(){
-    return this.http.get<IChange[]>(`${httpConfig.changesUrl}/today`)
-  }
-  getChange(type: IChangable) {
-    throw Error;
-    return this.http.get<IChange>(`${httpConfig.changesUrl}/`)
-  }
+  // getAllCurrentChanges(){
+  //   return this.http.get<IChange[]>(`${httpConfig.changesUrl}/today`)
+  // }
+  // getChange(type: IChangable) {
+  //   throw Error;
+  //   return this.http.get<IChange>(`${httpConfig.changesUrl}/`)
+  // }
 }
 
