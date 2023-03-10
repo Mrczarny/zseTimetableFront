63d0ba89dd6aaefdbb9d411a17ba580a85669ce0
mrczarny <j.adamski@zsebydgoszcz.onmicrosoft.com>
2023-03-10 12:54:30 +0100

    initial version of timetableView

--- src/app/timetable-view/timetable-view.component.ts
+++ src/app/timetable-view/timetable-view.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ISelectable } from '../Models/ISelectable';
+import { HttpClientService } from '../http-client.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { TimetableDay } from '../Models/TimetableDay';
 
 @Component({
   selector: 'app-timetable-view',
@@ -7,9 +12,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimetableViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private httpService: HttpClientService) {
+
+   }
+
+   SelectableName: string = '';
+   Selectable: ISelectable | null = null;
+   DataSource = new MatTableDataSource<TimetableDay>(this.Selectable?.Timetable.Days)
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {this.SelectableName = params['name']})
+    this.httpService.getTimetable(new ).subscribe(selecatable => this.Selectable = selecatable)
   }
 
 }
