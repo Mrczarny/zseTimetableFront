894e86eba259e6ed2a59dc33c2304252e360fe4f
Jan Adamski <j.adamski@zsebydgoszcz.onmicrosoft.com>
2023-03-14 09:49:36 +0100

    timetableView added LessonsRow

--- src/app/timetable-view/timetable-view.component.ts
+++ src/app/timetable-view/timetable-view.component.ts
@@ -4,6 +4,16 @@ import { ISelectable } from '../Models/ISelectable';
 import { HttpClientService } from '../http-client.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { TimetableDay } from '../Models/TimetableDay';
+import { Lesson } from '../Models/Lesson';
+import { Timetable } from '../Models/Timetable';
+
+interface LessonsRow {
+  mondayLesson: Lesson
+  tuesdayLesson: Lesson,
+  wednesdayLesson: Lesson,
+  thursdayLesson: Lesson,
+  fridayLesson: Lesson
+}
 
 @Component({
   selector: 'app-timetable-view',
@@ -19,12 +29,17 @@ export class TimetableViewComponent implements OnInit {
    SelectableName: string = '';
    Selectable: ISelectable | null = null;
    TypeName: string = ''
-   DataSource = new MatTableDataSource<TimetableDay>(this.Selectable?.Timetable.Days)
+   DataSource = new MatTableDataSource<LessonsRow>(this.ConvertToLessonRows(this.Selectable?.Timetable))
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {this.SelectableName = params['name']})
     this.route.url.subscribe(u => {this.TypeName = u.reverse()[1].path}) //TODO - no idea how urlsegment works
     this.httpService.getTimetable(this.TypeName, this.SelectableName).subscribe(selecatable => this.Selectable = selecatable)
+  }
+
+  ConvertToLessonRows(timetable: Timetable): LessonsRow[] {
+    throw new Error("Not implemented!");
+
   }
 
 }
