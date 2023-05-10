8607ff0d9a9b214a7db9e22b2cb12df6529b1ab4
Jan Adamski <j.adamski@zsebydgoszcz.onmicrosoft.com>
2023-05-10 02:05:13 +0200

    main select in timatable-select now supports autocomplete + pl translation

--- src/app/timetable-select/timetable-select.component.ts
+++ src/app/timetable-select/timetable-select.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+import {FormControl} from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClientService } from '../http-client.service';
 import { ISelectable } from '../Models/ISelectable';
 import { Teacher } from '../Models/Teacher';
 import { Class } from '../Models/Class';
 import { Classroom } from '../Models/Classroom';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-timetable-select',
@@ -14,19 +17,29 @@ import { Classroom } from '../Models/Classroom';
 })
 export class TimetableSelectComponent implements OnInit {
 
+  myControl = new FormControl('');
   typeName: String = '';
-  Names: string[] | null = null;
+  filteredNames: Observable<string[]> | undefined;
+  Names: string[] = [];
   selectedName: string =''
 
   constructor(
     private route: ActivatedRoute,
     private httpService: HttpClientService
-  ) { }
+  ) {
+    this.route.params.subscribe(params => {this.typeName = params['type']})
+    this.httpService.getAllNames(this.getType()).subscribe(names => {
+      this.Names = names;
+      this.filteredNames = this.myControl.valueChanges.pipe(
+        startWith(''),
+        map(value => this._filter(value || '')),);
+    })
+
+  }
 
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {this.typeName = params['type']})
-    this.httpService.getAllNames(this.getType()).subscribe(names => this.Names = names)
+
   }
 
   //TODO - this is ugly
@@ -39,5 +52,10 @@ export class TimetableSelectComponent implements OnInit {
       return 'class'
   }}
 
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.Names.filter(option => option.toLowerCase().includes(filterValue));
+  }
 
 }
